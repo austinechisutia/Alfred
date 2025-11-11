b51f3405d5cc7e66038eeca017b8b35999c119c2
AUstine <austineakhonya624@gmail.com>
2025-11-11 17:15:12 +0300

    name

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),  tailwindcss(),
   ],
+   base: '/Alfred/',
 })
